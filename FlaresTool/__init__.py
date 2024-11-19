198577a10e5b3d2c1b8aa00b1f8d4a9d4e65b960
Yuqi Kang <83055683+Kang-Yuqi@users.noreply.github.com>
2024-11-19 12:46:54 +1100

    Update __init__.py

--- FlaresTool/__init__.py
+++ FlaresTool/__init__.py
@@ -6,7 +6,6 @@ from . import gen_Gadget
 from . import plot_shell_arrange
 from . import box2dens
 from . import analytical_power_spectrum
-from . import lensing
 from . import nbody_analysis
 from . import nG_analysis
 from . import needlets
@@ -54,60 +53,3 @@ def read_config(filepath):
             print(f"Warning: Section '{section}' not found.")
 
     return params
-
-def read_param(fl_param):
-    Nside_map = fl_param['Nside_lensing']
-    Nside_shell = fl_param['Nside_shell']
-    num_shells = fl_param['num_shells']
-    n = sum(num_shells)
-    N = fl_param['Gaussian_shell_steps']
-    lmax = fl_param['lmax']
-    lmin = fl_param['lmin']
-    lmax_analysis = fl_param['lmax_analysis']
-    h = fl_param['H0'] / 100
-
-    root_local = fl_param['root_local_base'] + fl_param['root_unify']
-    root_cluster = fl_param['root_cluster_base'] + fl_param['root_unify']
-    
-    if fl_param['current_base'] == 'cluster':
-        root_local = root_cluster
-    elif fl_param['current_base'] == 'local':
-        pass
-    else:
-        raise TypeError("current_base should be 'local' or 'cluster'.")
-
-    try:
-        mixed_Nbody_folder_list = fl_param['mixed_Nbody_folder_list']
-        try:
-            mixed_Nbody_suffix = fl_param['mixed_Nbody_suffix']
-        except KeyError:
-            mixed_Nbody_suffix = f'mixed{len(mixed_Nbody_folder_list)}nbody'
-    except KeyError:
-        mixed_Nbody_folder_list = [fl_param['Nbody_folder']]
-        mixed_Nbody_suffix = fl_param['Nbody_folder']
-
-    # Return parameters as a dictionary
-    return {
-        'Nside_map': Nside_map,
-        'Nside_shell': Nside_shell,
-        'num_shells': num_shells,
-        'n': n,
-        'N': N,
-        'lmax': lmax,
-        'lmin': lmin,
-        'lmax_analysis':lmax_analysis,
-        'h': h,
-        'root_local': root_local,
-        'root_cluster': root_cluster,
-        'mixed_Nbody_folder_list':mixed_Nbody_folder_list,
-        'mixed_Nbody_suffix':mixed_Nbody_suffix
-    }
-
-def print_message(message,mpi):
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    if mpi:
-        if rank == 0:
-            print(message)
-    else:
-        print(message)
